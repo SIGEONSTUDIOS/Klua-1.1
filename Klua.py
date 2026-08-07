17851b34ef25f58007f1fc1754f1e7bd4121e3c2
sigeon studios <k8wvpygvsv@privaterelay.appleid.com>
2026-08-07 16:07:51 +0100

    added alot more stuff llike functions waiting and more
    added functions
    added better math
    added waiting (like time.sleep(int)

--- Klua.py
+++ Klua.py
@@ -1,11 +1,26 @@
+import time
+
+
 variables = {}
+functions = {}
+
+def do(name, code): 
+    functions[name] = code
+
+def run(name):
+    if name in functions:
+        functions[name]()
+    else:
+        print("Klua Error: Function not found")
 
 
-def get(name, text):
+
+
+def get(name, text): #input
     variables[name] = input(text)
     return variables[name]
 
-def make(name, value):
+def make(name, value): #variable creation
     try:
         if isinstance(value, bool):
             variables[name] = bool(value)
@@ -20,7 +35,7 @@ def make(name, value):
         print("Klua Error:", error)
 
 
-def kprint(text):
+def kprint(text): #output
     try:
         if text in variables:
             print(variables[text])
@@ -30,6 +45,34 @@ def kprint(text):
         print("Klua Error:", error)
         
 
-def condition(condition):
+def condition(condition): #conditions
     return eval(condition, {}, variables)
+
+
+def math(operator, name, amount): #math
+    try:
+        if operator == "add":
+            if name in variables:
+                variables[name] += amount
+            else:
+                print("Klua error: Variable not found")
+        elif operator == "minus":
+            if name in variables:
+                variables[name] -= amount
+            else:
+                print("Klua error: Variable not found")
+    except Exception as error:
+        print("Klua error:", error)
+
+
+def wait(amount): #waiting
+    try:
+        if isinstance(amount, bool):
+            print("Klua error: amount must be a number not a boolean")
+        elif isinstance(amount, int):
+            time.sleep(amount)
+        elif isinstance(amount, str):
+            print("Klua error: amount must be a number not a string")
+    except Exception as error:
+        print("Klua error:",error)
 
