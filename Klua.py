9fe3bd4d944f652e25c0e345283556c1c878a3cd
sigeon studios <k8wvpygvsv@privaterelay.appleid.com>
2026-08-07 15:15:14 +0100

    added lots of features
    ## Klua Update
    
    ### Added
    - Added input support
    - Added printing support
    - Added variable creation
    - Added string support
    - Added boolean support
    - Added integer support
    - Added error handling
    - Added conditional statements

---
+++ Klua.py
@@ -0,0 +1,35 @@
+variables = {}
+
+
+def get(name, text):
+    variables[name] = input(text)
+    return variables[name]
+
+def make(name, value):
+    try:
+        if isinstance(value, bool):
+            variables[name] = bool(value)
+        elif isinstance(value, int):
+            variables[name] = value
+        elif isinstance(value, str):
+            variables[name] = int(value)
+        else:
+            variables[name] = value
+        return variables[name]
+    except Exception as error:
+        print("Klua Error:", error)
+
+
+def Kprint(text):
+    try:
+        if text in variables:
+            print(variables[text])
+        else:
+            print(text)
+    except Exception as error:
+        print("Klua Error:", error)
+        
+
+def condition(condition):
+    return eval(condition, {}, variables)
+
